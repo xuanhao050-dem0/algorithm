a39c62857cd70e6ad885cd7afa0405f62665b4f5
xuanhao050-dem0 <xuanhao050@gmail.com>
2026-07-10 23:45:52 +0800

    day4

--- src/main/java/com/bit/writtenStrength/day4/Fibonacci.java
+++ src/main/java/com/bit/writtenStrength/day4/Fibonacci.java
@@ -1,4 +1,12 @@
 package com.bit.writtenStrength.day4;
 
+import java.util.Scanner;
+
 public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()){
+
+        }
+    }
 }
